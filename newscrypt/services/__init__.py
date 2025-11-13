585f8a4e438dd1b9cd3e9305ede2c22086abfc7c
James Richmond <jblackenmusic@gmail.com>
2025-11-13 11:39:41 -0700

    fix imports for package distribution

--- newscrypt/services/__init__.py
+++ newscrypt/services/__init__.py
@@ -1,8 +1,8 @@
-from newscrypt.services.news_service import NewsService
-from newscrypt.services.script_service import ScriptService
-from newscrypt.services.tts_service import TTSService
-from newscrypt.services.video_service import VideoService
-from newscrypt.services.subtitle_service import SubtitleService
+from .news_service import NewsService
+from .script_service import ScriptService
+from .tts_service import TTSService
+from .video_service import VideoService
+from .subtitle_service import SubtitleService
 
 __all__ = [
     "NewsService",
