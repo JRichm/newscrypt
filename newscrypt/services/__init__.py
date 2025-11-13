118d6ae2d12eab223be525a8d3552f76fd293af4
James Richmond <jblackenmusic@gmail.com>
2025-11-12 23:23:39 -0700

    update services __init__ file

---
+++ newscrypt/services/__init__.py
@@ -0,0 +1,13 @@
+from newscrypt.services.news_service import NewsService
+from newscrypt.services.script_service import ScriptService
+from newscrypt.services.tts_service import TTSService
+from newscrypt.services.video_service import VideoService
+from newscrypt.services.subtitle_service import SubtitleService
+
+__all__ = [
+    "NewsService",
+    "ScriptService",
+    "TTSService",
+    "VideoService",
+    "SubtitleService",
+]
